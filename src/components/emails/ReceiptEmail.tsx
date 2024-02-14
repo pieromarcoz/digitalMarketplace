0b19aa747cc0b867e2734e4074accc91a1a016d4
pieromarcoz <piero965d@gmail.com>
2024-02-14 02:00:36 -0500

    build

--- src/components/emails/ReceiptEmail.tsx
+++ src/components/emails/ReceiptEmail.tsx
@@ -14,6 +14,7 @@ import {
     Text,
     render,
 } from '@react-email/components'
+import * as React from 'react'
 import {formatPrice} from "../../lib/utils";
 import {format} from 'date-fns'
 interface ReceiptEmailProps  {
