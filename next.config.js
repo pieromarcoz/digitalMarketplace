49351f925dc178cd9d57e35e8ed348aaf14232f1
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-06-16 00:41:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
+        /*remotePatterns: [
             {
                 hostname: "localhost",
                 pathname: "**",
                 port: '3000',
                 protocol: 'http'
             }
+        ]*/
+        domains: [
+            'localhost',
+            'digitalmarketplace-pieromg.up.railway.app',
         ]
     }
 }
